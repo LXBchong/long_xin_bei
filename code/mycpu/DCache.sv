200d8edc3a3fd0f416e6841dcc20490d424d0bae
GOODChives <47296891+GOODChives@users.noreply.github.com>
2021-08-03 11:07:41 +0800

    without store buffer

--- code/mycpu/DCache.sv
+++ code/mycpu/DCache.sv
@@ -9,20 +9,19 @@ module DCache(
     output cbus_req_t dcreq,
     input cbus_resp_t dcresp
 );
-Dcache_offset_bits
 
-logic [3:0] meta_addr,data_raddr,data_waddr;
-logic [3:0] meta_addr_one,meta_addr_two;
-logic [3:0] data_raddr_one,data_raddr_two;
-logic [3:0] data_waddr_two;
+logic [Dcache_index_bits-1:0] meta_addr,data_raddr,data_waddr;
+logic [Dcache_index_bits-1:0] meta_addr_one,meta_addr_two;
+logic [Dcache_index_bits-1:0] data_raddr_one,data_raddr_two;
+logic [Dcache_index_bits-1:0] data_waddr_two;
 
 logic is_write;
 
 
-logic [3:0] meta_strobe,meta_strobe_two;
-logic [15:0] data_strobe,data_strobe_two; 
+logic [16:0] meta_strobe,meta_strobe_two;
+logic [63:0] data_strobe,data_strobe_two; 
 
-word_t r_meta,w_meta,w_meta_two,meta_two;
+word_t [Dcacheline_len-1:0] r_meta,w_meta,w_meta_two,meta_two;
 
 typedef logic [2:0] LRU_tag;
 LRU_tag [Dcache_set_num-1:0] LRU;
@@ -35,26 +34,29 @@ logic [Dcache_tag_bits-1:0] tag,tag_two;
 logic [Dcache_offset_bits -1:0] offset,offset_two;
 logic [Dcache_index_bits-1:0] index,index_two;
 
-logic hit_one,hit_two,new_hit_two;
-
 dbus_req_t dreq_two;
 
-logic [Dcache_index_bits-1:0] position_one;
+logic just_hit,hit,new_hit,hit_reg;
+
+logic [1:0] position,new_position,just_position,position_reg;
 
 logic stall_one,stall_two,resp_ok,resp_addr_ok,step_two_ok,uncached_two;
 
 logic last_pass;  // a new request go to step_two this cycle
 
-word_t [Dcacheline_len-1:0] data_reg,new_data,current_data,
-                            w_data,r_data,w_data_two;
+typedef word_t[Dcacheline_len-1:0] line_t;
+
+line_t[Dcache_way_num-1:0] w_data,r_data,w_data_two,
+                    data_reg,new_data,current_data;
 
 word_t new_word;
 
-logic [2:0] state_two,new_state_two;
+logic [2:0] state_two,new_state_two,state_two_reg;
 
-logic wb_addr,last_wb_addr;
+logic[31:0] wb_addr,last_wb_addr;
 
 D_LUTRAM  D_meta(.clk(clk),
+            .resetn(resetn),
             .addr(meta_addr),
             .strobe(meta_strobe),
             .wdata(w_meta),
@@ -92,70 +94,76 @@ assign cache_dresp.addr_ok  = resp_addr_ok;
 assign cache_dresp.data_ok  = resp_ok;
 
 assign cache_dresp.data     = uncached_two? 
-dcresp.data : new_data[offset_two[Dcache_offset_bits-1:2]];
+dcresp.data : new_data[position][offset_two[Dcache_offset_bits-1:2]];
 
-assign current_data = last_pass?r_data:data_reg;
+assign current_data     = last_pass?r_data:data_reg;
 
-
+assign meta_addr_one    = index;
+assign data_raddr_one   = index;
 //step one
+
+assign hit = last_pass? just_hit : hit_reg;
+assign position = last_pass? just_position : position_reg;
+assign state_two = last_pass?meta_two[position][31:29] : state_two_reg;
+
+
+//hit or miss & LRU
 always_comb begin
-    hit_one = '0;
-    position_one = '0;
+    just_hit = 1'b1;
+    just_position = '0;
     new_LRU_tag = '0;
-    for(int i=0;i<4;i++)begin
-        
-        meta_addr_one = {index,2'b00}+i;
-
-        if( tag == r_meta[Dcache_tag_bits-1:0] ) begin 
-            hit_one=1'b1;
-            position_one = i;
-            break;
+    
+    priority case(tag_two)
+        meta_two[0][Dcache_tag_bits-1:0]:just_position = 2'b00;
+        meta_two[1][Dcache_tag_bits-1:0]:just_position = 2'b01;
+        meta_two[2][Dcache_tag_bits-1:0]:just_position = 2'b10;
+        meta_two[3][Dcache_tag_bits-1:0]:just_position = 2'b11;
+        default: begin
+            just_position=2'b0;
+            just_hit=1'b0;
         end
-    end
+    endcase
 
-    if(~hit_one) begin
-        priority case(LRU[index])
+    if(~hit) begin
+        priority case(LRU[index_two])
             3'b000: begin
-                position_one    = 2'b00;  
+                just_position   = 2'b00;  
                 new_LRU_tag     = 3'b110;
             end
             3'b001: begin
-                position_one    = 2'b00;  
+                just_position   = 2'b00;  
                 new_LRU_tag     = 3'b111;
             end
             3'b010: begin 
-                position_one    = 2'b01;  
+                just_position   = 2'b01;  
                 new_LRU_tag     = 3'b100;
             end
             3'b011: begin 
-                position_one    = 2'b01;  
+                just_position   = 2'b01;  
                 new_LRU_tag     = 3'b101;
             end
             3'b100: begin 
-                position_one    = 2'b10;  
+                just_position   = 2'b10;  
                 new_LRU_tag     = 3'b001;
             end
             3'b101: begin 
-                position_one    = 2'b11;  
+                just_position   = 2'b11;  
                 new_LRU_tag     = 3'b000;
             end
             3'b110: begin 
-                position_one    = 2'b10;  
+                just_position   = 2'b10;  
                 new_LRU_tag     = 3'b011;
             end
             3'b111: begin 
-                position_one    = 2'b11;  
+                just_position   = 2'b11;  
                 new_LRU_tag     = 3'b010;
             end
             default:begin
-                position_one    = '0;  
+                just_position   = '0;  
                 new_LRU_tag     = '0;
             end
         endcase
     end
-
-    meta_addr_one   = {index,2'b00}+position_one;
-    data_raddr_one  = {index,2'b00}+position_one;
 end
 
 
@@ -173,19 +181,25 @@ dcreq.data      = '0;
 dcreq.len       = MLEN1;
 
 new_state_two   = `DONE;
+step_two_ok     = 1'b0;
 resp_ok         ='0;
 data_strobe_two ='0;
 meta_strobe_two ='0;
-w_data_two      ='0;
-w_meta_two      ='0;
+w_data_two      =current_data;
+
 new_word        ='0;
-new_data        ='0;
 last_wb_addr    ='0;
 
-if(dreq_two.valid & ~uncached_two)begin
-    if(hit_two) begin
+w_meta_two      = meta_two;
+new_position    = position;
+new_data        = current_data;
+new_hit         = '0;
 
-    new_hit_two     = 1'b1;
+if(dreq_two.valid & ~uncached_two)begin
+    
+    if(hit) begin
+
+    new_hit     = 1'b1;
     unique case(state_two)
 
         `INVALID:begin  
@@ -202,52 +216,52 @@ if(dreq_two.valid & ~uncached_two)begin
         end
         `VALID:begin
             resp_ok=1'b1;
+            w_meta_two[position] = {`VALID,29'b0}+tag_two;
             if(is_write)begin
-                new_data = current_data;
 
                 new_state_two   = `DONE;
 
-                data_strobe_two = 16'hffff;
-                meta_strobe_two = 4'b1111;
+                data_strobe_two = 64'hffffffffffffffff;
+                meta_strobe_two = 16'hffff;
 
-                w_data_two      = new_data;
-                w_meta_two      = {`DIRTY,'0,tag_two};
+                w_meta_two[position]    = {`DIRTY,29'b0}+tag_two;
 
-                new_word[7:0]  =dreq_two.strobe[0]?dreq_two.data[7:0]  :current_data[offset_two[Dcache_offset_bits-1:2]][7:0];
-                new_word[15:8] =dreq_two.strobe[1]?dreq_two.data[15:8] :current_data[offset_two[Dcache_offset_bits-1:2]][15:8];
-                new_word[23:16]=dreq_two.strobe[2]?dreq_two.data[23:16]:current_data[offset_two[Dcache_offset_bits-1:2]][23:16];
-                new_word[31:24]=dreq_two.strobe[3]?dreq_two.data[31:24]:current_data[offset_two[Dcache_offset_bits-1:2]][31:24];
+                new_word[7:0]  =dreq_two.strobe[0]?dreq_two.data[7:0]  :current_data[position][offset_two[Dcache_offset_bits-1:2]][7:0];
+                new_word[15:8] =dreq_two.strobe[1]?dreq_two.data[15:8] :current_data[position][offset_two[Dcache_offset_bits-1:2]][15:8];
+                new_word[23:16]=dreq_two.strobe[2]?dreq_two.data[23:16]:current_data[position][offset_two[Dcache_offset_bits-1:2]][23:16];
+                new_word[31:24]=dreq_two.strobe[3]?dreq_two.data[31:24]:current_data[position][offset_two[Dcache_offset_bits-1:2]][31:24];
 
-                new_data[offset_two[Dcache_offset_bits-1:2]] = new_word;
+                new_data[position][offset_two[Dcache_offset_bits-1:2]] = new_word;
+
+                w_data_two = new_data;
             end
         end
         `DIRTY:begin
             resp_ok=1'b1;
-            if(is_write)begin
-                new_data = current_data;
-                
-                new_state_two   = `DIRTY;
+            w_meta_two[position] = {`DIRTY,29'b0}+tag_two;
+            if(is_write)begin        
+                new_state_two   = `DONE;
 
-                data_strobe_two = 16'hffff;
-                meta_strobe_two = 4'b1111;
+                data_strobe_two = 64'hffffffffffffffff;
+                meta_strobe_two = 16'hffff;
 
-                w_data_two      = new_data;
-                w_meta_two      = {`DIRTY,'0,tag_two};
+            
+                w_meta_two[position]      = {`DIRTY,29'b0}+tag_two;
 
-                new_word[7:0]  =dreq_two.strobe[0]?dreq_two.data[7:0]  :current_data[offset_two[Dcache_offset_bits-1:2]][7:0];
-                new_word[15:8] =dreq_two.strobe[1]?dreq_two.data[15:8] :current_data[offset_two[Dcache_offset_bits-1:2]][15:8];
-                new_word[23:16]=dreq_two.strobe[2]?dreq_two.data[23:16]:current_data[offset_two[Dcache_offset_bits-1:2]][23:16];
-                new_word[31:24]=dreq_two.strobe[3]?dreq_two.data[31:24]:current_data[offset_two[Dcache_offset_bits-1:2]][31:24];
+                new_word[7:0]  =dreq_two.strobe[0]?dreq_two.data[7:0]  :current_data[position][offset_two[Dcache_offset_bits-1:2]][7:0];
+                new_word[15:8] =dreq_two.strobe[1]?dreq_two.data[15:8] :current_data[position][offset_two[Dcache_offset_bits-1:2]][15:8];
+                new_word[23:16]=dreq_two.strobe[2]?dreq_two.data[23:16]:current_data[position][offset_two[Dcache_offset_bits-1:2]][23:16];
+                new_word[31:24]=dreq_two.strobe[3]?dreq_two.data[31:24]:current_data[position][offset_two[Dcache_offset_bits-1:2]][31:24];
 
-                new_data[offset_two[Dcache_offset_bits-1:2]] = new_word;
+                new_data[position][offset_two[Dcache_offset_bits-1:2]] = new_word;
+                w_data_two = new_data;
+
             end
         end
         `READING:begin //wait
             if(dcresp.ready)begin
-                new_word = dresp.data;
-
-                new_data = current_data;
-                new_data[r_count] = new_word;
+                new_word = dcresp.data;
+                new_data[position][r_count] = new_word;
             end
             else begin
                 new_word='0;
@@ -273,10 +287,10 @@ if(dreq_two.valid & ~uncached_two)begin
             dcreq.size     = MSIZE4;
             dcreq.addr     = wb_addr;
             dcreq.strobe   = 4'b1111;
-            dcreq.data     = current_data[w_count];
+            dcreq.data     = current_data[position][w_count];
             dcreq.len      = MLEN4;
 
-            if(dcresp.last) new_state_two = `INVALID;
+            if(dcresp.last) new_state_two = `VALID;
             else new_state_two = `WRITING;
 
             last_wb_addr = dcreq.addr;
@@ -307,9 +321,10 @@ if(dreq_two.valid & ~uncached_two)begin
         dcreq.data     = '0;
         dcreq.len      = MLEN4;
 
+        new_hit = 1'b1;
+
         unique case(state_two)
         `INVALID:begin
-            new_hit_two=1'b1;
             new_state_two=`READING;
             //read from memeory
             dcreq.valid    = 1'b1;
@@ -322,7 +337,6 @@ if(dreq_two.valid & ~uncached_two)begin
         end
 
         `VALID:begin
-            new_hit_two = 1'b1;
             new_state_two = `READING;
             //read from memeory
             dcreq.valid    = 1'b1;
@@ -335,33 +349,38 @@ if(dreq_two.valid & ~uncached_two)begin
         end
 
         `DIRTY:begin
-            new_hit_two = 1'b1;
             new_state_two = `WRITING;
             dcreq.valid    = 1'b1;
             dcreq.is_write = 1'b1;
             dcreq.size     = MSIZE4;
-            dcreq.addr     = {meta_two[Dcache_tag_bits-1:0],index_two,'0};
+            dcreq.addr     = {meta_two[position][Dcache_tag_bits-1:0],index_two,'0};
             dcreq.strobe   = 4'b1111;
-            dcreq.data     = current_data[w_count];
+            dcreq.data     = current_data[position][w_count];
             dcreq.len      = MLEN4;
 
-            last_wb_addr = {meta_two[Dcache_tag_bits-1:0],index_two,'0};
+            last_wb_addr = {meta_two[position][Dcache_tag_bits-1:0],index_two,'0};
         end
         default:begin
-            new_state_two=state_two;
-            new_hit_two=1'b0;
+            new_state_two=`DONE;
+            new_hit=1'b0;
         end
         endcase             
     end
 end
 else if(dreq_two.valid & uncached_two)begin
-    new_state_two   = state_two;
+    new_state_two   = `DONE;
     new_data        = current_data;
-    new_hit_two     = 1'b0;
+    new_hit         = 1'b0;
     last_wb_addr    = '0;
 
-    if(dcresp.last) resp_ok= 1'b1;
-    else resp_ok    = 1'b0;
+    if(dcresp.last) begin 
+        resp_ok     = 1'b1;
+        step_two_ok = 1'b1;
+    end
+    else begin
+        resp_ok     = 1'b0;
+        step_two_ok = 1'b0;
+    end
 
     dcreq.valid     = 1'b1;
     dcreq.is_write  = is_write;
@@ -372,9 +391,10 @@ else if(dreq_two.valid & uncached_two)begin
     dcreq.len       = MLEN1;
 end
 else begin
-    new_state_two   = state_two;
+    step_two_ok     = 1'b1;
+    new_state_two   = `DONE;
     new_data        = current_data;
-    new_hit_two     = 1'b0;
+    new_hit         = 1'b0;
     last_wb_addr    = '0;
 
     dcreq.valid     = '0;
@@ -388,37 +408,34 @@ end
 end
 
 
-
-
 always_ff @(posedge clk) begin
     if(resetn)begin
         if(~stall_one) begin
-            last_pass   <=1'b1;
+            if(cache_dreq.valid) last_pass   <=1'b1;
 
             if(cache_dreq.valid) resp_addr_ok<=1'b1;
 
             dreq_two    <=cache_dreq;
             uncached_two<=d_uncached;
-            hit_two     <=hit_one;
             meta_two    <=r_meta;
-            state_two   <=r_meta[31:29];
 
             meta_addr_two   <=meta_addr_one;
             data_raddr_two  <=data_raddr_one;
-            data_waddr_two  <=data_raddr_one;
+            data_waddr_two  <=data_raddr_one;        
+        end
 
-            LRU[index]      <=new_LRU_tag;
-        end
-        else begin
-            last_pass   <=1'b0;
-            resp_addr_ok <=1'b0;
-        end
 
 
         if(stall_two)begin
-            data_reg    <=new_data;
-            hit_two     <=new_hit_two;
-            state_two   <=new_state_two;
+            if(last_pass) LRU[index]    <=new_LRU_tag;
+
+            data_reg        <=new_data;
+            hit_reg         <=new_hit;
+            position_reg    <=new_position;
+            state_two_reg   <=new_state_two;
+            
+            last_pass       <=1'b0;
+            resp_addr_ok    <=1'b0;
 
             if(dcreq.valid && ~dcreq.is_write && dcresp.ready ) r_count <= r_count + 1;
             else if(dcreq.valid && ~dcreq.is_write) ;
@@ -432,9 +449,11 @@ always_ff @(posedge clk) begin
             else wb_addr <='0;
         end
         else begin
-            data_reg    <='0;
-            hit_two     <='0;
-            state_two   <='0;
+            data_reg        <='0;
+            hit_reg         <='0;
+            position_reg    <='0;
+            state_two_reg   <='0;
+
             r_count     <='0;
             w_count     <='0;
             wb_addr     <='0;
@@ -442,22 +461,22 @@ always_ff @(posedge clk) begin
 
     end
     else begin
-        last_pass   <='0;
-        resp_addr_ok<='0;
-        dreq_two    <='0;
-        uncached_two<='0;
-        hit_two     <='0;
-        meta_two    <='0;
-        state_two   <=r_meta[31:29];
+        last_pass       <='0;
+        resp_addr_ok    <='0;
+        dreq_two        <='0;
+        uncached_two    <='0;
+        hit_reg         <='0;
+        position_reg    <='0;
+        meta_two        <='0;
+        state_two_reg   <='0;
+        data_reg        <='0;
 
         meta_addr_two   <='0;
         data_raddr_two  <='0;
         data_waddr_two  <='0;
         LRU<='0;
 
-        data_reg    <='0;
-        hit_two     <='0;
-        state_two   <='0;
+    
         r_count     <='0;
         w_count     <='0;
         wb_addr     <='0;
