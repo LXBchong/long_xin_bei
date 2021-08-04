0c053dd81f97914d113b40d1382ff353607763cf
GOODChives <47296891+GOODChives@users.noreply.github.com>
2021-08-05 03:19:26 +0800

    Update add_files.tcl

--- source/add_files.tcl
+++ source/add_files.tcl
@@ -1,9 +1,23 @@
 set COMMON_ROOT [file dirname [info script]]
 add_files [glob $COMMON_ROOT/include/*.svh]
 add_files $COMMON_ROOT/include/bus_decl
-add_files $COMMON_ROOT/util/
-add_files $COMMON_ROOT/ram/
-add_files $COMMON_ROOT/unit/
-add_files $COMMON_ROOT/mycpu/
-add_files $COMMON_ROOT/cache/
-add_files $COMMON_ROOT/external/
+
+add_files [glob $COMMON_ROOT/include/*.svh]
+add_files [glob $COMMON_ROOT/util/*.sv]
+# add_files [glob $COMMON_ROOT/ram/*.sv]
+add_files [glob $COMMON_ROOT/unit/*.sv]
+add_files [glob $COMMON_ROOT/mycpu/*.sv]
+# add_files [glob $COMMON_ROOT/cache/*.sv]
+add_files [glob $COMMON_ROOT/external/*.sv]
+
+add_files $COMMON_ROOT/cache/DCache.sv
+add_files $COMMON_ROOT/cache/ICache.sv
+add_files $COMMON_ROOT/cache/ICache_se.sv
+add_files $COMMON_ROOT/cache/LoadStoreBuffer.sv
+add_files $COMMON_ROOT/cache/MMU.sv
+add_files $COMMON_ROOT/cache/TU.sv
+
+add_files $COMMON_ROOT/ram/D_BRAM_v2.sv
+add_files $COMMON_ROOT/ram/D_LUTRAM_v2.sv
+add_files $COMMON_ROOT/ram/I_BRAM.sv
+add_files $COMMON_ROOT/ram/I_LUTRAM.sv
